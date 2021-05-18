f77b339f57a091372003622cb60f2f74aacf1952
Pasquale De Leo <p.deleo@almaviva.it>
2021-05-18 10:33:15 +0200

    InitialCommit

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ReadUrls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRep = new System.Windows.Forms.TextBox();
+            this.cmdStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmdStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtRep
+            // 
+            this.txtRep.Location = new System.Drawing.Point(0, 258);
+            this.txtRep.Multiline = true;
+            this.txtRep.Name = "txtRep";
+            this.txtRep.Size = new System.Drawing.Size(1243, 185);
+            this.txtRep.TabIndex = 0;
+            // 
+            // cmdStart
+            // 
+            this.cmdStart.Location = new System.Drawing.Point(24, 150);
+            this.cmdStart.Name = "cmdStart";
+            this.cmdStart.Size = new System.Drawing.Size(110, 39);
+            this.cmdStart.TabIndex = 4;
+            this.cmdStart.Text = "Start";
+            this.cmdStart.UseVisualStyleBackColor = true;
+            this.cmdStart.Click += new System.EventHandler(this.cmdStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Url:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(24, 37);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(1219, 22);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(24, 105);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(1219, 22);
+            this.txtFile.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Percorso e nome file:";
+            // 
+            // cmdStop
+            // 
+            this.cmdStop.Enabled = false;
+            this.cmdStop.Location = new System.Drawing.Point(152, 150);
+            this.cmdStop.Name = "cmdStop";
+            this.cmdStop.Size = new System.Drawing.Size(92, 39);
+            this.cmdStop.TabIndex = 5;
+            this.cmdStop.Text = "Stop";
+            this.cmdStop.UseVisualStyleBackColor = true;
+            this.cmdStop.Click += new System.EventHandler(this.cmdStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 450);
+            this.Controls.Add(this.cmdStop);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdStart);
+            this.Controls.Add(this.txtRep);
+            this.Name = "Form1";
+            this.Text = "Scarico XML";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRep;
+        private System.Windows.Forms.Button cmdStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cmdStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadUrls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool stop = false;
+        private void cmdStart_Click(object sender, EventArgs e)
+        {
+            FileStream fs = null;
+            cmdStart.Enabled = false;
+            cmdStop.Enabled = true;
+            try
+            { 
+                stop = false;
+                string lcUrl = txtUrl.Text;// @"http://sdmx.istat.it/SDMXWS/rest/data/IT1,41_993,1.0/A.../ALL/?detail=full&startPeriod=2006-01-01&endPeriod=2006-12-31&dimensionAtObservation=TIME_PERIOD";
+                HttpWebRequest loHttp =  (HttpWebRequest)WebRequest.Create(lcUrl);
+                loHttp.Timeout = 100000;     // 100 secs
+                loHttp.UserAgent = "Sample Web Client";
+                HttpWebResponse loWebResponse = (HttpWebResponse)loHttp.GetResponse();
+                Encoding enc = Encoding.GetEncoding(1252);   
+                StreamReader loResponseStream = new StreamReader(loWebResponse.GetResponseStream(), enc);
+                char[] buffer = new char[1024 * 1024];
+                int readed =0;
+                fs = File.Create(txtFile.Text);
+                //StringBuilder sb = new StringBuilder();
+                int totWrited = 0;
+                while ((readed=loResponseStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents();
+                    if (stop)
+                        break;
+                    //sb.Append(new string(buffer));
+                    string toWrite = (new string(buffer)).Substring(0,readed);
+                    fs.Write(Encoding.UTF8.GetBytes(toWrite),0, toWrite.Length);
+                    fs.Flush();
+                    totWrited += readed;
+                    txtRep.Text = " Caratteri letti: " + totWrited.ToString();
+                }
+                txtRep.Text +=Environment.NewLine+ " Fine elaborazione.";
+                fs.Close();
+                loWebResponse.Close();
+                loResponseStream.Close();
+            }
+            catch (Exception  ex)
+            {
+                if(fs!=null)
+                    fs.Close();
+                txtRep.Text = Environment.NewLine+ " Errore: " + ex.ToString();
+                cmdStart.Enabled = true;
+                cmdStop.Enabled = false;
+            }
+            cmdStart.Enabled = true;
+            cmdStop.Enabled = false;
+        }
+
+        private void cmdStop_Click(object sender, EventArgs e)
+        {
+            stop = true;
+            cmdStart.Enabled = true;
+            cmdStop.Enabled = false;
+
+        }
+    }
+}
